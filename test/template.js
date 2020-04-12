25b55be9b6cf550d5dd7e00f9df2dad12d1b8dff
Kobaivanov Yakir <yakirk1@gmail.com>
2020-04-12 18:32:27 +0300

    todo change

--- test/template.js
+++ test/template.js
@@ -1,6 +1,7 @@
 /*global Template, completedTodos*/
 Template.prototype.clearCompletedButton = function (completedTodos) {
     if (completedTodos > 0) {
+		completedTodos=completedTodos+1;
         return 'Clear completed';
     } else {
         return '';
