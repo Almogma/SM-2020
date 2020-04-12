d1c5dd8da8ae0979457bca0723627a09756b0ebe
Kobaivanov Yakir <yakirk1@gmail.com>
2020-04-12 19:07:53 +0300

    ?

--- test/template.js
+++ test/template.js
@@ -1,9 +1,16 @@
-/*global Template, completedTodos*/
-Template.prototype.clearCompletedButton = function (completedTodos) {
-    if (completedTodos > 0) {
-		completedTodos=completedTodos+1;
-        return 'Clear completed';
-    } else {
-        return '';
-    }
-};
+if (foo) {
+}
+
+while (foo) {
+}
+
+switch(foo) {
+}
+
+try {
+    doSomething();
+} catch(ex) {
+
+} finally {
+
+}
